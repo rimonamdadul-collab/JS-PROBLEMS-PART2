8fb7aacbfc4f35047732f0347fe62fb214674261
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-13 07:54:32 +0600

    add the wood

---
+++ wood.js
@@ -0,0 +1,15 @@
+function WoodQuantity(shirtQuantity,pantQuantity,shopeQuantity){
+    const shirtWood = 500;
+    const pantWood = 300;
+    const shopeWood = 1300;
+const shirt = shirtQuantity * shirtWood
+const pant = pantQuantity * pantWood
+const shope = shopeQuantity * shopeWood
+const total = shirt + pant+ shope
+
+return total
+}
+
+const Woods1 = WoodQuantity(3,2,1) 
+console.log(Woods1)    
+
